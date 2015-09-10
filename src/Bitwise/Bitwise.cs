0bb9e6a4f4dca18e62b6955f4d0409109b3d31e7
Le Huu Hoang Gia <hoanggia.lh@gmail.com>
2015-09-11 02:14:09 +0700

    Added Bitwise

---
+++ src/Bitwise/Bitwise.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace System.CommonLibraries.Bitwise
+{
+    public abstract class Bitwise<T> where T: struct
+    {
+        protected int Bit { get; set; }
+
+        protected Bitwise()
+        {
+            if (typeof(T).GetTypeInfo().BaseType != typeof(Enum) || typeof(T).GetTypeInfo().BaseType != typeof(int))
+            {
+                throw new InvalidCastException();
+            }
+        }
+
+        protected Bitwise(T bit)
+        {
+            Bit = Convert.ToInt32(bit);
+        }
+
+        public virtual bool Has(T bit)
+        {
+            return (Bit & Convert.ToInt32(bit)) > 0;
+        }
+
+        public virtual Bitwise<T> Set(T bit)
+        {
+            Bit = Convert.ToInt32(bit);
+            return this;
+        }
+
+        public virtual Bitwise<T> Add(T bit)
+        {
+            Bit = Bit | Convert.ToInt32(bit);
+            return this;
+        }
+
+        public virtual Bitwise<T> Remove(T bit)
+        {
+            var convertedBit = Convert.ToInt32(bit);
+            Bit = (Bit | convertedBit) & convertedBit;
+            return this;
+        }
+
+        public virtual IEnumerable<T> ToArray()
+        {
+            for (var i = 1; i <= Bit; i *= 2)
+            {
+                if ((i & Bit) <= 0) continue;
+
+                yield return (T)Enum.ToObject(typeof(T), i); ;
+            }
+        }
+    }
+}
---
+++ src/Bitwise/BitwiseInt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.CommonLibraries.Bitwise
+{
+
+    public class BitwiseInt : Bitwise<int>
+    {
+        public override IEnumerable<int> ToArray()
+        {
+            for (var i = 1; i <= Bit; i *= 2)
+            {
+                if ((i & Bit) <= 0) continue;
+                yield return i;
+            }
+        }
+    }
+}
